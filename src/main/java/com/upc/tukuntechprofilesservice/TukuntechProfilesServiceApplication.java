e6185cd1fe18de59d6d6117193a33de218c33e18
oscaredu1226 <oscaredu01@hotmail.com>
2025-11-13 20:33:22 -0500

    chore: initial commit

---
+++ src/main/java/com/upc/tukuntechprofilesservice/TukuntechProfilesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.upc.tukuntechprofilesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukuntechProfilesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TukuntechProfilesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/tukuntechprofilesservice/TukuntechProfilesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.tukuntechprofilesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukuntechProfilesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

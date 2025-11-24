744cc63ecb0853df694f670ce61595d0c6575a6f
oscaredu1226 <oscaredu01@hotmail.com>
2025-11-24 12:08:29 -0500

    feat(docs): add OpenAPI configuration with JWT bearer authentication

---
+++ src/main/java/com/upc/tukuntechprofilesservice/shared/config/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package com.upc.tukuntechprofilesservice.shared.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI profilesOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Tukuntech Profiles API")
+                        .description("Microservicio para gestión de perfiles (pacientes y cuidadores)")
+                        .version("v1")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
